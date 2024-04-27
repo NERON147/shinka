af6022bd6bef813198e0d25076741885b34dfc24
Maksim Bezzubenko <max.kolts2003@gamil.com>
2024-04-27 18:55:29 +0300

    fix

--- app/components/UI/Header.vue
+++ app/components/UI/Header.vue
@@ -18,27 +18,6 @@
           </a>
         </div>
       </div>
-      <div class="text-white text-[70px] font-bold mt-32 leading-[90px]">
-        ШИНОМОНТАЖ <br>
-        <span class="text-[#b87a3c]">
-          РЕМ. РАБОТЫ ЛЕГКОВЫХ
-        </span> <br> АВТОМОБИЛЕЙ В ГАТЧИНЕ
-      </div>
-      <div class="text-white font-light text-2xl tracking-[3px] mt-10">
-        Полный комплекс шиномонтажных <br>
-        работ от профессионалов
-      </div>
-      <div class="mt-24 flex gap-10 !font-light">
-        <Button
-          label="Оставить заявку"
-          class="bg-[#b87a3c] border-[#b87a3c] h-12"
-        />
-        <Button
-          label="Написать в Whatsapp"
-          class="text-white border-white h-12"
-          outlined
-        />
-      </div>
     </section>
 
   </header>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,6 +5,30 @@
       alt="bg img"
       class="w-[100vw] h-[100vh] object-cover absolute top-0 left-0 -z-10"
     >
+    <div class="container">
+      <div class="text-white text-[70px] font-bold mt-32 leading-[90px]">
+        ШИНОМОНТАЖ <br>
+        <span class="text-[#b87a3c]">
+          РЕМ. РАБОТЫ ЛЕГКОВЫХ
+        </span> <br> АВТОМОБИЛЕЙ В ГАТЧИНЕ
+      </div>
+      <div class="text-white font-light text-2xl tracking-[3px] mt-10">
+        Полный комплекс шиномонтажных <br>
+        работ от профессионалов
+      </div>
+      <div class="mt-24 flex gap-10 !font-light">
+        <Button
+          label="Оставить заявку"
+          class="bg-[#b87a3c] border-[#b87a3c] h-12"
+        />
+        <Button
+          label="Написать в Whatsapp"
+          class="text-white border-white h-12"
+          outlined
+        />
+      </div>
+    </div>
+
   </section>
 
 </template>
