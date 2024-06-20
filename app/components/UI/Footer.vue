898b1088331a80da259877530f7f6fcfaa42505b
Maksim Bezzubenko <max.kolts2003@gamil.com>
2024-06-20 17:58:58 +0300

    feat: add validate and toast

--- app/components/UI/Footer.vue
+++ app/components/UI/Footer.vue
@@ -13,17 +13,25 @@
         </p>
         <div class="mt-10 flex flex-col gap-5">
           <FloatLabel>
-            <InputText id="username" v-model="data.name" class="max-ss:w-full w-[500px] h-[50px]" />
+            <InputText id="username" v-model="data.name" class="max-ss:w-full w-[500px] h-[50px]" :invalid="v$.name.$error" />
             <label for="username">ФИО</label>
           </FloatLabel>
           <FloatLabel>
-            <InputMask id="phone" v-model="data.phone" class="max-ss:w-full w-[500px] h-[50px]" mask="+7 (999) 999 99 99" placeholder="99-999999" />
+            <InputMask
+              id="phone"
+              v-model="data.phone"
+              class="max-ss:w-full w-[500px] h-[50px]"
+              mask="+7 (999) 999 99 99"
+              placeholder="99-999999"
+              :invalid="v$.phone.$error"
+            />
             <label for="phone">Телефон</label>
           </FloatLabel>
           <Button
             label="Оставить заявку"
             class="bg-[#b87a3c] border-[#b87a3c] h-12 hover:bg-[#946435] hover:border-[#946435] max-ss:w-full w-[500px]"
             plain
+            :loading="data.loader"
             @click="sendMessage"
           />
           <div class="flex align-items-center mt-10">
@@ -37,35 +45,59 @@
         <img src="/assets/img/shina.png" alt="" class="absolute right-0 top-0 max-md:hidden">
       </div>
     </div>
+    <Toast position="bottom-left" group="bl" />
   </section>
 </template>
 
 <script setup lang="ts">
 import axios from 'axios'
+import { useToast } from 'primevue/usetoast'
+import { useVuelidate } from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+const toast = useToast()
 const data = reactive({
   name: '',
   phone: '',
-  apply: false
+  apply: false,
+  loader: false
 })
 
+const rules = {
+  name: { required },
+  phone: { required }
+}
+
+const v$ = useVuelidate(rules, data)
+
 const sendMessage = async () => {
-  const orderToTG = `
+  v$.value.$validate()
+  if (v$.value.$error) {
+    toast.add({ severity: 'error', summary: 'Ошибка!', detail: 'Заполните все обязатльные поля!', group: 'bl', life: 6000 })
+  } else {
+    data.loader = true
+    const orderToTG = `
   <b> Новая заявка: </b> \n
   <b> Имя: </b>  ${data.name}
   <b> Телефон: </b>  ${data.phone} \n 
   `
-  const chatId = '-1002238302043'
-  await axios.post('https://api.telegram.org/bot7282294081:AAFefQZH9b4CxTQeUv1RPbIBduUnoqTbrVk/sendMessage', {
-    chat_id: chatId,
-    parse_mode: 'html',
-    text: orderToTG
-  })
-    .then(() => {
-      console.log('norm')
+    const chatId = '-1002238302043'
+    await axios.post('https://api.telegram.org/bot7282294081:AAFefQZH9b4CxTQeUv1RPbIBduUnoqTbrVk/sendMessage', {
+      chat_id: chatId,
+      parse_mode: 'html',
+      text: orderToTG
     })
-    .catch((err) => {
-      console.log(err)
-    })
+      .then(() => {
+        data.name = ''
+        data.phone = ''
+        v$.value.$reset()
+        data.loader = false
+        toast.add({ severity: 'success', summary: 'Успешно!', detail: 'Заявка отправлена успешно, в ближайшее время наш сотрудник Вам перезвонит, спасибо за доверие!', group: 'bl', life: 6000 })
+      })
+      .catch(() => {
+        data.loader = false
+        toast.add({ severity: 'error', summary: 'Ошибка!', detail: 'Упс, произошла ошибка, попробуйте еще раз через некоторе время, либо позвоните нам сами!', group: 'bl', life: 6000 })
+      })
+  }
 }
 </script>
 
