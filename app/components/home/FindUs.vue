d35804ed1e43293efec9e4e69eb4012a35adae9b
Maksim Omich <108236279+NERON147@users.noreply.github.com>
2025-03-15 16:50:32 +0300

    fix: fix link

--- app/components/home/FindUs.vue
+++ app/components/home/FindUs.vue
@@ -9,7 +9,7 @@
           <i class="pi pi-compass text-primary text-4xl" />
           <a
             class="max-md:text-xl text-2xl font-bold"
-            href="yandexnavi://maps.yandex.ru/?url=https://yandex.ru/maps/-/CDf-f6pr"
+            href="yandexnavi://maps.yandex.ru/?url=https://yandex.ru/maps/org/shinny_tsentr/11583592634/?ll=30.162460%2C59.551708&z=17"
             target="_blank"
           >
             станция Фрезерная, 1, посёлок Торфяное, <br> Гатчинский муниципальный округ, Ленинградская область
